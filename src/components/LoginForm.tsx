33506dde48ab313bd9cbf8e707a070477a1526c8
vap1 <vaibhavvasant1705@gmail.com>
2023-08-31 20:50:07 +0530

    First Commit

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -2,7 +2,7 @@
 import React, { useState } from 'react';
 import { View, TextInput, Button, StyleSheet } from 'react-native';
 import { UserLoginRequest } from '../types/Types';
-import AuthApi from '../apis/AuthApi';
+import { loginUser } from '../apis/AuthApi';
 
 const LoginForm: React.FC = () => {
   const [email, setEmail] = useState('');
@@ -15,7 +15,7 @@ const LoginForm: React.FC = () => {
         password,
       };
 
-      const response = await AuthApi.loginUser(request);
+      const response = await loginUser(request);
       console.log(response); // Handle the response as per your requirement
     } catch (error) {
       console.error(error); // Handle the error as per your requirement
