d6d67fc0c8e680f243c37ced8ff9fc8d633ae731
vap1 <vaibhavvasant1705@gmail.com>
2023-08-31 20:43:48 +0530

    First Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,20 @@
 
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-
+import { SafeAreaView, StatusBar } from 'react-native';
 import { ThemeProvider } from './contexts/ThemeContext';
 import { AuthProvider } from './contexts/AuthContext';
+import AppNavigator from './navigation/AppNavigator';
 
-import RegistrationScreen from './screens/RegistrationScreen';
-import LoginScreen from './screens/LoginScreen';
-import ProfileScreen from './screens/ProfileScreen';
-import AdminUserDetailsScreen from './screens/AdminUserDetailsScreen';
-
-const Stack = createStackNavigator();
-const Tab = createBottomTabNavigator();
-
-const AppNavigator = () => {
+const App: React.FC = () => {
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="Registration" component={RegistrationScreen} />
-      <Stack.Screen name="Login" component={LoginScreen} />
-      <Stack.Screen name="Main" component={MainNavigator} options={{ headerShown: false }} />
-    </Stack.Navigator>
-  );
-};
-
-const MainNavigator = () => {
-  return (
-    <Tab.Navigator>
-      <Tab.Screen name="Profile" component={ProfileScreen} />
-      <Tab.Screen name="AdminUserDetails" component={AdminUserDetailsScreen} />
-    </Tab.Navigator>
-  );
-};
-
-const App = () => {
-  return (
-    <NavigationContainer>
-      <ThemeProvider>
-        <AuthProvider>
+    <ThemeProvider>
+      <AuthProvider>
+        <SafeAreaView style={{ flex: 1 }}>
+          <StatusBar barStyle="dark-content" />
           <AppNavigator />
-        </AuthProvider>
-      </ThemeProvider>
-    </NavigationContainer>
+        </SafeAreaView>
+      </AuthProvider>
+    </ThemeProvider>
   );
 };
 
