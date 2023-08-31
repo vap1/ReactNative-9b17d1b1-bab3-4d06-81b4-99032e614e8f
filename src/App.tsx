9ed83f7c0d4e5297d4a364efa6dcecb355157327
vap1 <vaibhavvasant1705@gmail.com>
2023-08-31 21:02:46 +0530

    First Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,16 @@
 
 import React from 'react';
 import { SafeAreaView, StatusBar } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { AuthProvider } from './contexts/AuthContext';
-import RegistrationScreen from './screens/RegistrationScreen';
-import LoginScreen from './screens/LoginScreen';
-import ProfileScreen from './screens/ProfileScreen';
-import AdminUserDetailsScreen from './screens/AdminUserDetailsScreen';
-
-const Stack = createStackNavigator();
+import AppNavigator from './navigation/AppNavigator';
 
 const App: React.FC = () => {
   return (
-    <NavigationContainer>
-      <AuthProvider>
-        <StatusBar barStyle="dark-content" />
-        <SafeAreaView style={{ flex: 1 }}>
-          <Stack.Navigator>
-            <Stack.Screen name="Registration" component={RegistrationScreen} />
-            <Stack.Screen name="Login" component={LoginScreen} />
-            <Stack.Screen name="Profile" component={ProfileScreen} />
-            <Stack.Screen name="AdminUserDetails" component={AdminUserDetailsScreen} />
-          </Stack.Navigator>
-        </SafeAreaView>
-      </AuthProvider>
-    </NavigationContainer>
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView style={{ flex: 1 }}>
+        <AppNavigator />
+      </SafeAreaView>
+    </>
   );
 };
 
