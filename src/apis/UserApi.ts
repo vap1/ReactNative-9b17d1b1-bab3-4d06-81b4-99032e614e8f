7fdef474b4b0e19f3d809b28238b89b896577e49
vap1 <vaibhavvasant1705@gmail.com>
2023-08-31 21:27:26 +0530

    First Commit

--- src/apis/UserApi.ts
+++ src/apis/UserApi.ts
@@ -4,11 +4,15 @@ import { UserRegistrationRequest, UserRegistrationResponse } from '../types/Type
 
 const BASE_URL = 'https://api.example.com';
 
-export const registerUser = async (request: UserRegistrationRequest): Promise<UserRegistrationResponse> => {
+const registerUser = async (request: UserRegistrationRequest): Promise<UserRegistrationResponse> => {
   try {
     const response = await axios.post(`${BASE_URL}/api/register`, request);
     return response.data;
   } catch (error) {
-    throw new Error(error.response.data.message);
+    throw new Error('Failed to register user');
   }
+};
+
+export default {
+  registerUser,
 };
