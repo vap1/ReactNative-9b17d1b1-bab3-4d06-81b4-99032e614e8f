61023ed829a889ab1081aaaf9c040157a7a5a019
vap1 <vaibhavvasant1705@gmail.com>
2023-08-31 21:33:40 +0530

    First Commit

--- src/screens/RegistrationScreen.tsx
+++ src/screens/RegistrationScreen.tsx
@@ -1,14 +1,13 @@
 
 import React, { useState } from 'react';
 import { View, TextInput, Button, StyleSheet } from 'react-native';
-import { UserRegistrationRequest, UserRegistrationResponse } from '../types/Types';
-import UserApi from '../apis/UserApi';
+import { UserRegistrationRequest } from '../types/Types';
+import { registerUser } from '../apis/UserApi';
 
 const RegistrationScreen: React.FC = () => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
 
   const handleRegistration = async () => {
     try {
@@ -18,14 +17,8 @@ const RegistrationScreen: React.FC = () => {
         password,
       };
 
-      const response: UserRegistrationResponse = await UserApi.registerUser(request);
-      if (response.success) {
-        // Registration successful, navigate to the next screen
-        // Replace 'NextScreen' with the actual screen component
-        // navigation.navigate('NextScreen');
-      } else {
-        setErrorMessage(response.message);
-      }
+      const response = await registerUser(request);
+      console.log(response); // Handle the response as per your requirement
     } catch (error) {
       console.error(error); // Handle the error as per your requirement
     }
@@ -53,7 +46,6 @@ const RegistrationScreen: React.FC = () => {
         onChangeText={setPassword}
       />
       <Button title="Register" onPress={handleRegistration} />
-      {errorMessage ? <Text style={styles.error}>{errorMessage}</Text> : null}
     </View>
   );
 };
@@ -70,10 +62,6 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     marginBottom: 16,
     paddingHorizontal: 8,
-  },
-  error: {
-    color: 'red',
-    marginBottom: 16,
   },
 });
 
