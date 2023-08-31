e2b5f4162c3bc7b81cd19aeed7753b307ad7e0bb
vap1 <vaibhavvasant1705@gmail.com>
2023-08-31 21:20:55 +0530

    First Commit

--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -1,39 +1,24 @@
 
 import React, { useEffect, useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-import { UserProfileRequest, UserProfileResponse, UserProfileUpdateRequest, UserProfileUpdateResponse, User } from '../types/Types';
-import ProfileApi from '../apis/ProfileApi';
-import ProfileForm from '../components/ProfileForm';
+import { UserProfileRequest, UserProfileResponse, User } from '../types/Types';
+import { getUserProfile } from '../apis/ProfileApi';
 
 const ProfileScreen: React.FC = () => {
   const [user, setUser] = useState<User | null>(null);
 
   useEffect(() => {
-    const fetchUserProfile = async () => {
-      try {
-        const request: UserProfileRequest = {
-          token: 'YOUR_AUTH_TOKEN', // Replace with the actual token
-        };
-
-        const response: UserProfileResponse = await ProfileApi.getUserProfile(request);
-        setUser(response.user);
-      } catch (error) {
-        console.error(error); // Handle the error as per your requirement
-      }
-    };
-
     fetchUserProfile();
   }, []);
 
-  const handleProfileUpdate = async (updatedUser: User) => {
+  const fetchUserProfile = async () => {
     try {
-      const request: UserProfileUpdateRequest = {
-        token: 'YOUR_AUTH_TOKEN', // Replace with the actual token
-        ...updatedUser,
+      const request: UserProfileRequest = {
+        token: 'JWT_TOKEN', // Replace with the actual JWT token
       };
 
-      const response: UserProfileUpdateResponse = await ProfileApi.updateUserProfile(request);
-      console.log(response); // Handle the response as per your requirement
+      const response: UserProfileResponse = await getUserProfile(request);
+      setUser(response.user);
     } catch (error) {
       console.error(error); // Handle the error as per your requirement
     }
@@ -41,10 +26,20 @@ const ProfileScreen: React.FC = () => {
 
   return (
     <View style={styles.container}>
-      {user ? (
-        <ProfileForm user={user} onProfileUpdate={handleProfileUpdate} />
-      ) : (
-        <Text>Loading...</Text>
+      <Text style={styles.heading}>User Profile</Text>
+      {user && (
+        <View style={styles.userContainer}>
+          <Text style={styles.label}>Name:</Text>
+          <Text>{user.name}</Text>
+          <Text style={styles.label}>Email:</Text>
+          <Text>{user.email}</Text>
+          <Text style={styles.label}>Contact Info:</Text>
+          <Text>{user.contactInfo}</Text>
+          <Text style={styles.label}>Address:</Text>
+          <Text>{user.address}</Text>
+          <Text style={styles.label}>Profile Picture:</Text>
+          <Text>{user.profilePicture}</Text>
+        </View>
       )}
     </View>
   );
@@ -55,6 +50,17 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     paddingHorizontal: 16,
+  },
+  heading: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 16,
+  },
+  userContainer: {
+    marginBottom: 16,
+  },
+  label: {
+    fontWeight: 'bold',
   },
 });
 
