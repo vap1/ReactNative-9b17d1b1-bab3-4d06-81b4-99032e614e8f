4322c249c2ba0115358da07a38a55393236d46f6
vap1 <vaibhavvasant1705@gmail.com>
2023-08-31 21:27:31 +0530

    First Commit

--- src/apis/ProfileApi.ts
+++ src/apis/ProfileApi.ts
@@ -4,20 +4,25 @@ import { UserProfileRequest, UserProfileResponse, UserProfileUpdateRequest, User
 
 const BASE_URL = 'https://api.example.com';
 
-export const getUserProfile = async (request: UserProfileRequest): Promise<UserProfileResponse> => {
+const getUserProfile = async (request: UserProfileRequest): Promise<UserProfileResponse> => {
   try {
     const response: AxiosResponse<UserProfileResponse> = await axios.get(`${BASE_URL}/api/profile`, { headers: { Authorization: `Bearer ${request.token}` } });
     return response.data;
   } catch (error) {
-    throw new Error(error.response?.data?.message || 'Failed to get user profile');
+    throw new Error('Failed to get user profile');
   }
 };
 
-export const updateUserProfile = async (request: UserProfileUpdateRequest): Promise<UserProfileUpdateResponse> => {
+const updateUserProfile = async (request: UserProfileUpdateRequest): Promise<UserProfileUpdateResponse> => {
   try {
     const response: AxiosResponse<UserProfileUpdateResponse> = await axios.put(`${BASE_URL}/api/profile`, request, { headers: { Authorization: `Bearer ${request.token}` } });
     return response.data;
   } catch (error) {
-    throw new Error(error.response?.data?.message || 'Failed to update user profile');
+    throw new Error('Failed to update user profile');
   }
+};
+
+export default {
+  getUserProfile,
+  updateUserProfile,
 };
