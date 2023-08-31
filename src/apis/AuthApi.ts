2af340118cdad59af72c85454f0a9b1132fe9477
vap1 <vaibhavvasant1705@gmail.com>
2023-08-31 21:14:32 +0530

    First Commit

--- src/apis/AuthApi.ts
+++ src/apis/AuthApi.ts
@@ -4,11 +4,15 @@ import { UserLoginRequest, UserLoginResponse } from '../types/Types';
 
 const BASE_URL = 'https://api.example.com';
 
-export const loginUser = async (request: UserLoginRequest): Promise<UserLoginResponse> => {
+const loginUser = async (request: UserLoginRequest): Promise<UserLoginResponse> => {
   try {
     const response = await axios.post(`${BASE_URL}/api/login`, request);
     return response.data;
   } catch (error) {
-    throw new Error(error.response.data.message);
+    throw new Error('Failed to login user');
   }
+};
+
+export default {
+  loginUser,
 };
