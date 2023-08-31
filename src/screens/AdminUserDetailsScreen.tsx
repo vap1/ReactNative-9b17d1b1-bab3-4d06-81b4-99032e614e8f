5a00d84b6eb32181fcd29b2afd9ddc907e15c082
vap1 <vaibhavvasant1705@gmail.com>
2023-08-31 20:43:42 +0530

    First Commit

--- src/screens/AdminUserDetailsScreen.tsx
+++ src/screens/AdminUserDetailsScreen.tsx
@@ -1,45 +1,71 @@
 
 import React, { useEffect, useState } from 'react';
-import { View, Text, FlatList } from 'react-native';
-import { AdminUserDetailsResponse, User } from '../types/Types';
-import { getAdminUserDetails } from '../apis/AdminApi';
+import { View, Text, StyleSheet } from 'react-native';
+import { AdminUserDetailsRequest, AdminUserDetailsResponse, User } from '../types/Types';
+import AdminApi from '../apis/AdminApi';
 
 const AdminUserDetailsScreen: React.FC = () => {
   const [users, setUsers] = useState<User[]>([]);
 
   useEffect(() => {
+    const fetchAdminUserDetails = async () => {
+      try {
+        const request: AdminUserDetailsRequest = {
+          token: 'YOUR_ADMIN_AUTH_TOKEN', // Replace with the actual admin token
+        };
+
+        const response: AdminUserDetailsResponse = await AdminApi.getAdminUserDetails(request);
+        setUsers(response.users);
+      } catch (error) {
+        console.error(error); // Handle the error as per your requirement
+      }
+    };
+
     fetchAdminUserDetails();
   }, []);
 
-  const fetchAdminUserDetails = async () => {
-    try {
-      const response: AdminUserDetailsResponse = await getAdminUserDetails();
-      setUsers(response.users);
-    } catch (error) {
-      console.error('Error fetching admin user details:', error);
-    }
-  };
-
-  const renderUserItem = ({ item }: { item: User }) => (
-    <View>
-      <Text>Name: {item.name}</Text>
-      <Text>Email: {item.email}</Text>
-      <Text>Contact Info: {item.contactInfo}</Text>
-      <Text>Address: {item.address}</Text>
-      <Text>Profile Picture: {item.profilePicture}</Text>
-    </View>
-  );
-
   return (
-    <View>
-      <Text>Admin User Details</Text>
-      <FlatList
-        data={users}
-        renderItem={renderUserItem}
-        keyExtractor={(item) => item.email}
-      />
+    <View style={styles.container}>
+      <Text style={styles.heading}>Admin User List</Text>
+      {users.map((user) => (
+        <View key={user.email} style={styles.userContainer}>
+          <Text style={styles.userEmail}>{user.email}</Text>
+          <Text style={styles.userName}>{user.name}</Text>
+          <Text style={styles.userContact}>{user.contactInfo}</Text>
+          <Text style={styles.userAddress}>{user.address}</Text>
+        </View>
+      ))}
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingHorizontal: 16,
+  },
+  heading: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 16,
+  },
+  userContainer: {
+    marginBottom: 16,
+  },
+  userEmail: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  userName: {
+    fontSize: 14,
+  },
+  userContact: {
+    fontSize: 14,
+  },
+  userAddress: {
+    fontSize: 14,
+  },
+});
 
 export default AdminUserDetailsScreen;
