18e9d0523da452e898a1f60159954e10796e927a
vap1 <vaibhavvasant1705@gmail.com>
2023-08-31 20:43:41 +0530

    First Commit

--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -1,79 +1,104 @@
 
-import React, { useState, useEffect } from 'react';
-import { View, Text, TextInput, Button, Image } from 'react-native';
-import { UserProfileRequest, UserProfileResponse, UserProfileUpdateRequest, UserProfileUpdateResponse } from '../types/Types';
-import { getUserProfile, updateUserProfile } from '../apis/ProfileApi';
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { UserProfileRequest, UserProfileResponse, UserProfileUpdateRequest, UserProfileUpdateResponse, User } from '../types/Types';
+import ProfileApi from '../apis/ProfileApi';
 
 const ProfileScreen: React.FC = () => {
+  const [user, setUser] = useState<User | null>(null);
   const [name, setName] = useState('');
   const [contactInfo, setContactInfo] = useState('');
   const [address, setAddress] = useState('');
   const [profilePicture, setProfilePicture] = useState('');
-  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
+    const fetchUserProfile = async () => {
+      try {
+        const request: UserProfileRequest = {
+          token: 'YOUR_AUTH_TOKEN', // Replace with the actual token
+        };
+
+        const response: UserProfileResponse = await ProfileApi.getUserProfile(request);
+        setUser(response.user);
+      } catch (error) {
+        console.error(error); // Handle the error as per your requirement
+      }
+    };
+
     fetchUserProfile();
   }, []);
 
-  const fetchUserProfile = async () => {
-    try {
-      const request: UserProfileRequest = {
-        token: 'YOUR_JWT_TOKEN', // Replace with actual JWT token
-      };
-
-      const response: UserProfileResponse = await getUserProfile(request);
-      setName(response.user.name);
-      setContactInfo(response.user.contactInfo || '');
-      setAddress(response.user.address || '');
-      setProfilePicture(response.user.profilePicture || '');
-      setIsLoading(false);
-    } catch (error) {
-      console.error('Error fetching user profile:', error);
-    }
-  };
-
-  const handleSaveProfile = async () => {
+  const handleUpdateProfile = async () => {
     try {
       const request: UserProfileUpdateRequest = {
-        token: 'YOUR_JWT_TOKEN', // Replace with actual JWT token
+        token: 'YOUR_AUTH_TOKEN', // Replace with the actual token
         name,
         contactInfo,
         address,
         profilePicture,
       };
 
-      const response: UserProfileUpdateResponse = await updateUserProfile(request);
-      console.log('Profile updated successfully:', response.message);
+      const response: UserProfileUpdateResponse = await ProfileApi.updateUserProfile(request);
+      console.log(response); // Handle the response as per your requirement
     } catch (error) {
-      console.error('Error updating user profile:', error);
+      console.error(error); // Handle the error as per your requirement
     }
   };
 
-  if (isLoading) {
+  if (!user) {
     return (
-      <View>
+      <View style={styles.container}>
         <Text>Loading...</Text>
       </View>
     );
   }
 
   return (
-    <View>
-      <Text>Name:</Text>
-      <TextInput value={name} onChangeText={setName} />
-
-      <Text>Contact Info:</Text>
-      <TextInput value={contactInfo} onChangeText={setContactInfo} />
-
-      <Text>Address:</Text>
-      <TextInput value={address} onChangeText={setAddress} />
-
-      <Text>Profile Picture:</Text>
-      <Image source={{ uri: profilePicture }} style={{ width: 100, height: 100 }} />
-
-      <Button title="Save" onPress={handleSaveProfile} />
+    <View style={styles.container}>
+      <Text style={styles.heading}>Profile</Text>
+      <Text>Name: {user.name}</Text>
+      <Text>Email: {user.email}</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Contact Info"
+        value={contactInfo}
+        onChangeText={setContactInfo}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Address"
+        value={address}
+        onChangeText={setAddress}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Profile Picture"
+        value={profilePicture}
+        onChangeText={setProfilePicture}
+      />
+      <Button title="Update Profile" onPress={handleUpdateProfile} />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingHorizontal: 16,
+  },
+  heading: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 16,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 16,
+    paddingHorizontal: 8,
+  },
+});
 
 export default ProfileScreen;
